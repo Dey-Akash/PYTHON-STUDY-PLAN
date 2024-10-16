9e0ba5b46ce9e1e52e8ec5fbded0654299ac6969
AKASH DEY <deyakash264@gmail.com>
2024-10-16 21:39:13 +0530

    Update Fibonacci series.py

--- Fibonacci series.py
+++ Fibonacci series.py
@@ -1,14 +1,12 @@
 # FIBONACCI SERIES :--->         #(XXXXXXXXXXXXXXXXXXXXXXXX)
 
-# a = int(input("enter A: "))
-# b = int(input("enter B: "))
+# a = 0
+# b = 1
 # num = int(input("enter a number: "))
 
 # if num ==1:
 #     print(a)
 # else:
-#     print(a)
-#     print(b)
 #     for i in range (1,num+1):
 #         c = a+b
 #         a = b
