86d8333f74e34b67b39ac878b215b4d8c3eba21e
AKASH DEY <deyakash264@gmail.com>
2024-10-16 21:39:59 +0530

    Update Fibonacci series.py

--- Fibonacci series.py
+++ Fibonacci series.py
@@ -13,12 +13,3 @@
 #         b = c
 #         print(c)
         
-# reverse a number or string:----->
-
-string_no = input("enter string number: ")
-reverse_string = string_no[::-1]
-print("reverse no: ",reverse_string)
-
-# it's a extra mathematical adition process
-ans = int(reverse_string)+1
-print("ANS: ",ans)
